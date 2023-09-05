5b34917eb375f21949943727417e640ef04188b1
sykul <luke.flatley@outlook.com>
2023-09-05 22:36:34 +0200

    remove extraneous check for num == 0

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,17 +1,13 @@
 const repeatString = function(string, num) {
     let output = '';
-    if (num == 0) {
-        return output;
-    }   else if (num<0) {
+    if (num<0) {
         return 'ERROR';
     } else {
         for (let i = 0; i<num; i++) {
             output = `${output}${string}`;
-            if (i == num-1) {
-                return output;
-            }
+        }
+        return output;
     }
-}
 };
 
 // Do not edit below this line
