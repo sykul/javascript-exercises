0c40f2cb38401a437717a1a00afe25c9a3978c8a
sykul <luke.flatley@outlook.com>
2023-09-05 21:34:23 +0200

    Write repeatString function to pass jest tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let output = '';
+    if (num == 0) {
+        return output;
+    }   else if (num<0) {
+        return 'ERROR';
+    } else {
+        for (let i = 0; i<num; i++) {
+            output = `${output}${string}`;
+            if (i == num-1) {
+                return output;
+            }
+    }
+}
 };
 
 // Do not edit below this line
