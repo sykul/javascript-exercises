4f814d20e96d7ab752ec805ee0244af0317da9d9
sykul <luke.flatley@outlook.com>
2023-09-05 23:11:40 +0200

    Reverse input strings

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    splitArray = string.split('');
+    reversedArray = splitArray.reverse();
+    stringFromArray = reversedArray.join('');
+    return stringFromArray;
 };
 
 // Do not edit below this line
