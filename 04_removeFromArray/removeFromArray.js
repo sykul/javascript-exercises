44c1e321a5a04b38d2a8e144ad3ff204cca483fb
sykul <luke.flatley@outlook.com>
2023-09-06 20:20:42 +0200

    Remove a single value

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,6 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, removeThis) {
+    array.splice(array.indexOf(removeThis),1);
+    return array;
 };
 
 // Do not edit below this line
