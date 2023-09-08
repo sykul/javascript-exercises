307731a83d6a237940346e9427b62dd67e5d2ad5
sykul <luke.flatley@outlook.com>
2023-09-08 22:28:30 +0200

    Create calculator functions

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+const add = function(a,b) {
+  return a+b;	
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(numberArray) {
+	let total = 0;
+  numberArray.forEach((number) => total += number);
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = function(numberArray) {
+  let total = 1;
+  numberArray.forEach((number) => total *= number);
+  return total;
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+  return a**b;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+	if (number == 0) {
+    return 1;
+  } else {
+    let result = 1;
+    while (number > 0) {
+      result = number*result;
+      number--;
+    }
+    return result;
+  }
 };
 
 // Do not edit below this line
