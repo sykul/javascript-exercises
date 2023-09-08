68a2c02bc4edc9008f09de3e8fbb506c3c57c4ac
sykul <luke.flatley@outlook.com>
2023-09-08 14:26:33 +0200

    sum numbers between inputted numbers

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function(first, last) {
+    if (first > last) {
+        [first, last] = [last, first];
+    }
 
+    if (first < 0 || last < 0 || !Number.isInteger(first) || !Number.isInteger(last)) {
+        return 'ERROR';
+    }
+
+    let runningTotal = 0;
+    while (first <= last) {
+        runningTotal += first;
+        first++;
+    }
+    
+    return runningTotal;
 };
 
 // Do not edit below this line
