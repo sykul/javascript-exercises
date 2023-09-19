3eaeb17ff3afe8fd1ad3a767dd181ca51b2fb5bd
sykul <luke.flatley@outlook.com>
2023-09-19 23:41:30 +0200

    check for palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let stringArray = string.split('');
 
+    for (i=0; i<stringArray.length/2; i++) {
+        if (stringArray[i] == stringArray[stringArray.length-1-i]) {
+            continue;
+        } else {
+            return "no";
+        }
+    }
+        return true;
 };
 
 // Do not edit below this line
