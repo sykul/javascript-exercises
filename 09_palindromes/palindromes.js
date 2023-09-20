66a1972c4098197ff60a1bdab2fb97389b03b5e6
sykul <luke.flatley@outlook.com>
2023-09-20 15:31:23 +0200

    test for palindrome

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,11 +1,15 @@
 const palindromes = function (string) {
-    let stringArray = string.split('');
+    let stringArray = string
+    .replace(/[!"#$%&'()*+, -./:;<=>?@[\]^_`{|}~]/g, '')
+    .toLowerCase()
+    .split('');
+
 
     for (i=0; i<stringArray.length/2; i++) {
         if (stringArray[i] == stringArray[stringArray.length-1-i]) {
             continue;
         } else {
-            return "no";
+            return false;
         }
     }
         return true;
