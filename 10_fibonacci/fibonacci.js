3672031d3daf0d68b7087c689f61f22e52c08a12
sykul <luke.flatley@outlook.com>
2023-09-20 16:22:52 +0200

    return fibonacci numbers

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if (num < 0) {
+        return "OOPS";
+    }
+    n = 1;
+    nPlusOne = 1;
+    nMinusOne = 1;
+    for (i=1; i<num; i++) {
+        nPlusOne = nMinusOne + n;
+        nMinusOne = n;
+        n = nPlusOne;
+    }
+    return nMinusOne;
 };
 
 // Do not edit below this line
