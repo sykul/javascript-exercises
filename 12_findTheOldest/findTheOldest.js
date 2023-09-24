5957fc3167200f96ae38ed4dfd5b9b2822327263
sykul <luke.flatley@outlook.com>
2023-09-24 11:06:44 +0200

    find age of each person at death or current year

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,9 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    const currentYear = new Date().getFullYear();
+    people.forEach(person => {
+        const age = person.yearOfDeath ? person.yearOfDeath - person.yearOfBirth :  currentYear - person.yearOfBirth;
+        console.log(`${person.name}: ${age}`);
+    })
 };
 
 // Do not edit below this line
