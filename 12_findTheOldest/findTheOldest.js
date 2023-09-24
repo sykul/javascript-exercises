33ade90ac07e8e7deacbb4ab57f3f48d318652c5
sykul <luke.flatley@outlook.com>
2023-09-24 13:36:47 +0200

    calculate and add age to each person

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -2,8 +2,9 @@ const findTheOldest = function(people) {
     const currentYear = new Date().getFullYear();
     people.forEach(person => {
         const age = person.yearOfDeath ? person.yearOfDeath - person.yearOfBirth :  currentYear - person.yearOfBirth;
-        console.log(`${person.name}: ${age}`);
+        Object.defineProperty(person, 'age', {value: age, writable: false,});
     })
+    return people;
 };
 
 // Do not edit below this line
